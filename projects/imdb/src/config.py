f3b4bd001de51d87260d23bcf0585c1a5f221475
Py4mac <boisbu@gmail.com>
2021-01-08 07:48:15 +0100

    On-going stuff related to NLP

---
+++ projects/imdb/src/config.py
@@ -0,0 +1,3 @@
+# Input file: https://raw.githubusercontent.com/TheClub4/IMDB_Sentiment_Analysis/master/movie_data.csv
+TRAINING_FILE = "./projects/imdb/inputs/imdb.csv"
+MODEL_OUTPUT = "./projects/imdb/models/"
